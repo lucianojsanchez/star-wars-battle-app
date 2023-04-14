069f2e64ff422a87f8e13e9a335760e849bddf74
Luciano Sanchez <cbrook3n@gmail.com>
2023-04-14 01:37:17 -0300

    versus changes

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { useState, CSSProperties } from "react";
+import { css } from "@emotion/react";
+import { ClipLoader } from "react-spinners";
+
 type Props = {
   characters: {
     name: string;
@@ -9,18 +14,47 @@ type Props = {
     cybernetics: string;
     homeworld: string;
   };
+  hotReload: boolean;
+  setHotReload: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const Card = ({ characters }: Props) => {
+const Card = ({ characters, hotReload, setHotReload }: Props) => {
+  const [loading, setLoading] = useState(true);
+
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
+  if (hotReload) {
+    setHotReload(false);
+    window.location.reload();
+  }
+
   return (
-    <div className="group pt-5">
+    <div className="group py-2">
       <div className="overflow-hidden flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-0">
+        <ClipLoader
+          color={"#000"}
+          loading={loading}
+          cssOverride={override}
+          size={150}
+        />
         <img
           src={characters.image}
           alt={characters.name}
-          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          className={`${
+            loading ? "hidden" : ""
+          } object-cover  rounded-t-lg md:h-64 md:w-48 md:rounded-none md:rounded-l-lg max-w-64
+          `}
+          onLoad={() => setLoading(false)}
         />
-        <div className="overflow-hidden flex flex-col justify-between p-4 leading-normal ">
+        <div
+          className={`${
+            loading ? "hidden" : ""
+          } overflow-hidden flex flex-col justify-between leading-normal px-2 m-1`}
+        >
           <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {characters.name}
           </h2>
--- components/Versus.tsx
+++ components/Versus.tsx
@@ -1,13 +1,46 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import fetchCharacters from "@/utils/fetchCharacters";
+
+interface Character {
+  name: string;
+  image: string;
+  species: string;
+  mass: string;
+  height: string;
+  manufacturer: string;
+  cybernetics: string;
+  homeworld: string;
+  // add more properties here if needed
+}
 
 const Versus = () => {
   const [character1, setCharacter1] = useState("");
   const [character2, setCharacter2] = useState("");
-  const [characters1Data, setCharacters1Data] = useState(null);
-  const [characters2Data, setCharacters2Data] = useState(null);
+  const [characters1Data, setCharacters1Data] = useState<Character | null>(
+    null
+  );
+  const [characters2Data, setCharacters2Data] = useState<Character | null>(
+    null
+  );
+  const [hotReload, setHotReload] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const characters = await fetchCharacters();
+      const character1Data = characters.find((character: { name: string }) =>
+        character.name.toLowerCase().includes("luke")
+      );
+      const character2Data = characters.find((character: { name: string }) =>
+        character.name.toLowerCase().includes("vader")
+      );
+      setCharacters1Data(character1Data);
+      setCharacters2Data(character2Data);
+    };
+    fetchData();
+  }, []);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -18,8 +51,34 @@ const Versus = () => {
     const character2Data = characters.find((character: { name: string }) =>
       character.name.toLowerCase().includes(character2.toLowerCase())
     );
+
+    if (!character1Data) {
+      alert(`Character 1 "${character1}" not found`);
+      return;
+    }
+
+    if (!character2Data) {
+      alert(`Character 2 "${character2}" not found`);
+      return;
+    }
+
     setCharacters1Data(character1Data);
     setCharacters2Data(character2Data);
+  };
+  const handleRandomCharacter = async (cardNumber: number) => {
+    const characters = await fetchCharacters();
+    const randomCharacter =
+      characters[Math.floor(Math.random() * characters.length)];
+
+    if (cardNumber === 1) {
+      setCharacters1Data(randomCharacter);
+    } else {
+      setCharacters2Data(randomCharacter);
+    }
+
+    console.log(
+      `Character 1: ${characters1Data?.name}, Character 2: ${characters2Data?.name}`
+    );
   };
 
   return (
@@ -32,6 +91,9 @@ const Versus = () => {
           onChange={(e) => setCharacter1(e.target.value)}
           className="text-black"
         />
+        <div>
+          <h3>VS</h3>
+        </div>
         <input
           type="text"
           placeholder="Character 2"
@@ -43,9 +105,19 @@ const Versus = () => {
       </form>
       {characters1Data && characters2Data && (
         <div className="">
-          <Card characters={characters1Data} />
-          <h1>Vs</h1>
-          <Card characters={characters2Data} />
+          <Card
+            characters={characters1Data}
+            hotReload={hotReload}
+            setHotReload={setHotReload}
+          />
+          <button onClick={() => handleRandomCharacter(1)}>Random</button>
+          <h1 className="m-5">Versus</h1>
+          <Card
+            characters={characters2Data}
+            hotReload={hotReload}
+            setHotReload={setHotReload}
+          />
+          <button onClick={() => handleRandomCharacter(2)}>Random</button>
         </div>
       )}
     </div>
