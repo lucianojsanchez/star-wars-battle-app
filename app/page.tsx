f1076c4e7096a347bc02b77473098c2703fab08b
Luciano Sanchez <cbrook3n@gmail.com>
2023-04-16 06:41:07 -0300

    landing page changes, styles and pagination added

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,9 @@ const Home = () => {
             </h1>
             <p className="text-[#f0f0f0] text-base sm:text-lg md:text-xl mb-12 md:mb-16 leading-relaxed max-w-4xl mx-auto text-center font-source">
               Explore epic battles between iconic characters from the Star Wars
-              universe and discover new ones! Let's delve into the thrilling
-              world of lightsabers, the Force, and intergalactic warfare.
+              universe and discover new ones! Let&apos;s delve into the
+              thrilling world of lightsabers, the Force, and intergalactic
+              warfare.
             </p>
             <Link href="/characters">
               <button className="bg-yellow-400 hover:bg-yellow-500 shadow-[#EEDB00]/50 shadow-lg text-gray-900 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ">
